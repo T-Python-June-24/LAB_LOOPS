b140dabbcdf3f8396b848d109440e682e0ebd628
Abdulrahman-Alwagdani <167430546+Abdulrahman-Alwagdani@users.noreply.github.com>
2024-06-10 11:39:37 +0300

    Completed LAB_LOOPS

---
+++ LAB_LOOPS.py
@@ -0,0 +1,13 @@
+numbers:list = range(45, 210)
+for number in numbers:
+    if number == 100:
+        continue
+    if number == 205:
+        break
+    print(number)
+
+problem:str = "What is the product of 7 * 24?"
+while input(problem) != 7 * 24:
+    print("Your Answer is wrong try again...")
+else:
+    print("You answered this Question correctly")
