96ae4d220e6116778ef5a14d68c0fe9c30f142ac
Sarah <monifsarah@gmail.com>
2024-06-10 12:18:39 +0300

    Resolve Bonus added the check if positive feature

--- Bonus.py
+++ Bonus.py
@@ -1,9 +1,21 @@
+'''
+while True :
+    try:  
+        numbers= int(input("Enter a positive integer : "))
+        print("integer ")
+        break
+    except ValueError:
+        print("invalid input, please enter a number")
+'''
 numbers= int(input("Enter a positive integer : "))
-numbers_range=range(1,numbers+1)
-sum:int=0
-for i in numbers_range :
-    if i%2==0:
-       sum = sum+i
-    else :
-        continue
+while numbers<0 :
+    numbers =int (input("invalid input, please enter a positive number : "))
+else :
+      numbers_range=range(1,numbers+1)
+      sum:int=0
+      for i in numbers_range :
+         if i%2==0:
+           sum = sum+i
+         else :
+          continue
 print(f"The sum of even numbers between 1 and {numbers} is {sum} . ")
